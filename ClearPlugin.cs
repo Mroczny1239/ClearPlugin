6a5e936244a5312183cac84b35021ac87588c51c
Mroczny1239 <55148693+Mroczny1239@users.noreply.github.com>
2021-03-21 19:34:19 +0100

    Add files via upload

---
+++ ClearPlugin.cs
@@ -0,0 +1,44 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace Mroczny.ClearPlugin
+{
+    public class ClearPlugin : RocketPlugin<ClearPluginConfiguration>
+    {
+        public static ClearPlugin Instance;
+        string Version = "1.0.3";
+        string Creator = "Mroczny";
+        public Color MessageColor { get; set; }
+
+        protected override void Load()
+        {
+            Instance = this;
+            MessageColor = UnturnedChat.GetColorFromName(Configuration.Instance.MessageColor, Color.green);
+            Logger.LogWarning($"Loading {Name} {Version}...");
+            Logger.LogWarning($"Plugin Made By {Creator}");
+            Logger.LogWarning($"{Name} has been loaded!");
+        }
+
+        protected override void Unload()
+        {
+            Instance = null;
+            Logger.LogWarning($"{Name} has been unloaded!");
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList(){
+            {"ClearInventorySuccess","Inventory cleared!"},
+            {"ClearInventoryPlayerSuccess","Player's {0} inventory has been cleared!"},
+            {"PlayerNotFound","Player not found!"},
+            {"ClearItemsSuccess","All items cleared!"},
+            {"ClearVehiclesSuccess","All vehicles cleared!"}
+        };
+    }
+}
---
+++ ClearPluginConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Rocket.API;
+
+namespace Mroczny.ClearPlugin
+{
+    public class ClearPluginConfiguration : IRocketPluginConfiguration
+    {
+        public string MessageColor;
+
+        public void LoadDefaults()
+        {
+            MessageColor = "magenta";
+        }
+    }
+}
