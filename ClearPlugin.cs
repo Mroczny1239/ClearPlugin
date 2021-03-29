c80083763666e86507deafbeeb40e6ff3ac9ddaa
Mroczny1239 <55148693+Mroczny1239@users.noreply.github.com>
2021-03-29 12:00:19 +0200

    Added automatic vehicles clear

--- ClearPlugin.cs
+++ ClearPlugin.cs
@@ -9,7 +9,7 @@ namespace Mroczny.ClearPlugin
     public class ClearPlugin : RocketPlugin<ClearPluginConfiguration>
     {
         public static ClearPlugin Instance { get; private set; }
-        const string Version = "1.0.4";
+        const string Version = "1.0.5";
         const string Creator = "Mroczny";
         public Color MessageColor { get; set; }
 
@@ -19,12 +19,17 @@ namespace Mroczny.ClearPlugin
             MessageColor = UnturnedChat.GetColorFromName(Configuration.Instance.MessageColor, Color.green);
             Logger.LogWarning($"{Name} {Version} has been loaded!");
             Logger.LogWarning($"Plugin Made By {Creator}");
+            if (Configuration.Instance.AutoClearVehiclesEnabled)
+            {
+                InvokeRepeating("ClearVehicles", Configuration.Instance.AutoClearVehiclesIntervalSeconds, Configuration.Instance.AutoClearVehiclesIntervalSeconds);
+            }
         }
 
         protected override void Unload()
         {
             Instance = null;
             Logger.LogWarning($"{Name} has been unloaded!");
+            CancelInvoke("ClearVehicles");
         }
 
         public override TranslationList DefaultTranslations => new TranslationList(){
@@ -34,5 +39,11 @@ namespace Mroczny.ClearPlugin
             {"ClearItemsSuccess","All items cleared!"},
             {"ClearVehiclesSuccess","All vehicles cleared!"}
         };
+        
+        public void ClearVehicles()
+        {
+            VehicleManager.askVehicleDestroyAll();
+            Logger.LogWarning("All vehicles cleared!");
+        }
     }
 }
