8fa8c39ab8268780e471919652baf02ab22cf287
Mroczny1239 <55148693+Mroczny1239@users.noreply.github.com>
2021-03-29 11:59:11 +0200

    Added automatic vehicles clear

--- ClearPluginConfiguration.cs
+++ ClearPluginConfiguration.cs
@@ -5,10 +5,14 @@ namespace Mroczny.ClearPlugin
     public class ClearPluginConfiguration : IRocketPluginConfiguration
     {
         public string MessageColor { get; set; }
+        public bool AutoClearVehiclesEnabled { get; set; }
+        public float AutoClearVehiclesIntervalSeconds { get; set; }
 
         public void LoadDefaults()
         {
             MessageColor = "magenta";
+            AutoClearVehiclesEnabled = true;
+            AutoClearVehiclesIntervalSeconds = 300f;
         }
     }
 }
